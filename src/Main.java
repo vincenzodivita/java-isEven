f315fc5c1e9dd94bb2c0fe8b0af4d7b94f17fc2a
vincenzodivita <divitavincenzo@icloud.com>
2023-08-04 02:12:58 +0200

    Ask user a number

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,14 @@ public class Main {
         // Import Scanner
         Scanner scanner = new Scanner(System.in);
 
-        // Let's code!
+        // Ask user a number
+        System.out.println("Ciao! Scegli un numero e ti dirò se è pari o dispari");
+        int userChoice = scanner.nextInt();
+        scanner.nextLine();
+
+
+        
+        System.out.println("Hai scelto il numero " + userChoice);
     
     }
 }
